907ab0b4efd6865b40915570e25f6b02f54566b7
Pedro Armando <pedro.arrmando@gmail.com>
2026-06-08 23:37:54 -0300

    novo audio artes para o jogo Readme atualizado

---
+++ projeto_2/README.md
@@ -0,0 +1,8 @@
+Campo Minado é um jogo antigo, um classíco dos jogos digitais, e desde sua criação em 1989 ele foi recriado das maneiras mais criativas possíveis.
+Existem versões com o campo em formatos diferentes (2D, 3D, 6D, Hexagonal etc.), e com objetivos únicos como a versão inspirada em RPG medieval.
+Tendo isso em vista, o objetivo do nosso grupo é recriar o código do campo minado clássico, mas de maneira a tornar possível a sua utilização para criar versões mais criativas do jogo.
+
+
+Código: Python e Pygame
+
+Em decorrencia da utilização do Pygame para aplicar o front-end, as expansões possíveis do código fonte terão foco no que pode ser abstraido ao 2D.
---
+++ projeto_2/src/view/janela.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+
+def criar_janela():
+    # Inicializa o Pygame
+    pygame.init()
+
+    # Configurações da janela
+    largura = 800
+    altura = 600
+    tela = pygame.display.set_mode((largura, altura))
+    pygame.display.set_caption("Janela Básica Pygame")
+
+    # Cores
+    COR_FUNDO = (30, 30, 30)  # Cinza escuro
+    COR_CIRCULO = (0, 255, 128) # Verde água
+
+    # Loop principal
+    rodando = True
+    while rodando:
+        # Gerenciamento de eventos
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                rodando = False
+
+        # Desenho
+        tela.fill(COR_FUNDO)
+        
+        # Desenha um círculo no centro apenas para ter algo visual
+        pygame.draw.circle(tela, COR_CIRCULO, (largura // 2, altura // 2), 50)
+        pygame.draw.rect(tela, (255, 0, 0), ((largura - (largura// 4)) , 0 , (largura // 4), altura)) # Desenha um retângulo vermelho no lado direito da tela Para representar a área de informações do jogo
+
+        # Atualiza a tela
+        pygame.display.flip()
+    # Encerra o Pygame
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    criar_janela()
